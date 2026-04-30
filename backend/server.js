da916b86287de5eb2ecb0d852209758e19133d4d
Himanshu <himanshuswami770@gmail.com>
2026-04-30 15:39:12 +0530

    port changes

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,8 @@ app.get("/", (req, res) => {
 
 
 
-app.listen(8000, () => {
-  console.log("Server running on port 8000");
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
